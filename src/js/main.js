880737c95049b1a9475fce558b0dceec2d53f417
Artem <xthemey@gmail.com>
2021-03-23 23:09:43 +0300

    pc styles done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-	
-})
+	const menuBurger = document.querySelector('.toggle-menu');
+	const navmenu = document.querySelector('.navmenu');
+
+	menuBurger.addEventListener('click', function () {
+		this.classList.toggle('active');
+		navmenu.classList.toggle('active');
+	});
+
+	//Клик в окне навигации
+	navmenu.addEventListener('click', function () {
+		this.classList.remove('active');
+		menuBurger.classList.remove('active');
+	});
+
+	//Ресайз окна
+	window.addEventListener('resize', function () {
+		navmenu.classList.remove('active');
+		menuBurger.classList.remove('active');
+	});
+
+	$("#navAccordion").hover(function () {
+		$(".submenu").slideToggle('fast');
+	});
+
+});
